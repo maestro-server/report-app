6add9ba2767bc9f8c8e27e295dd513356a0d2818
Felipe Signorini <fsignorini@rdisoftware.com>
2018-02-20 14:51:05 +0000

    bugfix notification

--- app/tasks/notification.py
+++ app/tasks/notification.py
@@ -8,7 +8,7 @@ def task_notification(self, report_id, msg, status='success', more={}):
     data = {'_id': report_id, 'status': status, 'msg': msg}
     merged = {**data, **more}
 
-    path = FactoryURL.make(path="reports", resource="MAESTRO_URL")
+    path = FactoryURL.make(path="reports")
     ret = requests.put(path, json={'body': [merged]})
 
     return {'name': self.request.task, 'report_id': report_id, 'status': status}
