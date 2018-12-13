bc916f6efcfe1434e8a8b38e64b8654a5c0e7ffc
Felipe Signorini <felipe.signorini@capgemini.com>
2018-12-13 13:54:03 -0200

    add url with slashs

--- app/views.py
+++ app/views.py
@@ -13,11 +13,11 @@ from .controller import HomeApp, GeneralReport, PivotReport, ReportsApp, ReportS
 api = Api(app)
 
 api.add_resource(HomeApp, '/')
-api.add_resource(GeneralReport, '/reports/general')
-api.add_resource(PivotReport, '/reports/pivot')
+api.add_resource(GeneralReport, '/reports/general', '/reports/general/')
+api.add_resource(PivotReport, '/reports/pivot', '/reports/pivot/')
 api.add_resource(ReportsApp, '/reports')
-api.add_resource(ReportSingleApp, '/reports/<table_name>')
-api.add_resource(ReportAggregationApp, '/aggregation/<table_name>')
+api.add_resource(ReportSingleApp, '/reports/<table_name>', '/reports/<table_name>/')
+api.add_resource(ReportAggregationApp, '/aggregation/<table_name>', '/aggregation/<table_name>/')
 
 
 @app.errorhandler(404)
