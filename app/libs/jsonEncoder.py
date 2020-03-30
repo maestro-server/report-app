5ea7d221369d1837772be80e4f549d9e491769f3
Felipe Signorini <felipeklerk@yahoo.com.br>
2020-03-30 20:19:43 +0100

    date json encode

--- app/libs/jsonEncoder.py
+++ app/libs/jsonEncoder.py
@@ -8,7 +8,11 @@ class DateTimeEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, ObjectId):
             return str(obj)
-        if isinstance(obj, datetime) or (type(obj) is pd.Timestamp):
+        if isinstance(obj, datetime.date):
+            return obj.isoformat()
+        if (type(obj) is pd.Timestamp):
+            return obj.isoformat()
+        if isinstance(obj, datetime.datetime):
             return obj.isoformat()
 
         return json.JSONEncoder.default(self, obj)
